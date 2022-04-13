eef425d55103b5c22047a67f0dd8d52014abe623
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-13 16:15:46 +0800

    Add files via upload

---
+++ 10-6.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+
+struct node{
+    int val;
+    struct node *next;
+};
+
+typedef struct node NODE;
+
+void insert(NODE **, int);
+void traverse(NODE **);
+NODE* midddle(NODE **);
+NODE* middleII(NODE **);
+
+
+int main(){
+    NODE *h = NULL;
+    insert(&h, 1);
+    insert(&h, 2);
+    insert(&h, 3);
+    insert(&h, 4);
+    insert(&h, 5);
+    //insert(&h, 6);
+
+    traverse(&h);
+
+    NODE *m = middleII(&h);
+
+    traverse(&m);
+}
+
+
+/*If even: Return prior middle*/
+NODE* middleII(NODE **h){
+    if(!(*h)) return;
+
+
+    NODE *slow = *h;
+    NODE *fast = (*h)->next;
+
+    while(fast){
+        fast = fast->next;
+        if(fast){
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+
+    return slow;
+}
+
+/*If even: Return post middle*/
+NODE* midddle(NODE **h){
+    if(!(*h)) return;
+
+    NODE *fast = *h;
+    NODE *slow = *h;
+
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+void traverse(NODE **h){
+    if(!(*h)) return;
+
+    NODE *c = *h;
+
+    while(c){
+        printf("%d ", c->val);
+        c = c->next;
+    }
+
+    printf("\n");
+}
+
+void insert(NODE **h, int val){
+    NODE *n = (NODE *)malloc(sizeof(NODE));
+    n->val = val;
+    n->next = NULL;
+
+    if(!(*h))
+        *h = n;
+    else{
+        NODE *c = *h;
+        while(c->next){
+            c = c->next;
+        }
+        c->next = n;
+    }
+}
