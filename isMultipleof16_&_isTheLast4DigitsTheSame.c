45d80c8916cd357c64c041aec72abffbf2c803f1
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-13 12:58:01 +0800

    Update isMultipleof16_&_isTheLast4DigitsTheSame.c

--- isMultipleof16_&_isTheLast4DigitsTheSame.c
+++ isMultipleof16_&_isTheLast4DigitsTheSame.c
@@ -9,10 +9,12 @@ int main(){
     return 0;
 }
 
+/*N is Multiplied by 16*/
 void isMultipleof16(int n){
     printf("%d is multiple of 16: %s\n", n, (n >= 16 && !(n & 15))?"True":"False");
 }
 
+/*給你一個unsigned short(0~65535)，問換算成16進制後四個值是不是一樣，是回傳1，否則0。例如：0xFFFF return 1, 0xAAAB return 0*/
 bool is16theSame(unsigned short n){
     /*use the mask to extract the last 4 digits*/
     int mask = n&15;
