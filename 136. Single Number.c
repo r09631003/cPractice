51a5282091d9c55f943a2ffb46c06950a42a5d09
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-13 11:38:42 +0800

    Add files via upload

---
+++ 136. Single Number.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+/*Leetcode 136. Single Number*/
+int singleNumber(int* nums, int numsSize){
+    int tmp = nums[0];
+
+    for(int i = 1; i < numsSize; i++){
+        tmp ^= nums[i];
+    }
+
+    return tmp;
+}
+
+/*Leetcode Single Number III*/
+
+void singleNumberIII(int *nums, int numsSize){
+    int tmp = nums[0];
+    int ans[2];
+
+    ans[0] = 0;
+    ans[1] = 0;
+
+    /*XOR all the number in array. will get the number represents the diff between the 2 unique number*/
+    for(int i = 1; i < numsSize; i++){
+        tmp ^= nums[i];
+    }
+
+    /*the mask will find the last 1. in tmp, and the 1. represent the last different bits in the two unique number*/
+    tmp &= (-tmp);
+
+
+    /*divide the nums into 2 part: has / has no same bit with mask*/
+    for(int i = 0; i < numsSize; i++){
+        if(nums[i] & tmp)        /*if nums & mask == 0, it means that it has no same bit with mask*/
+            ans[0] ^= nums[i];
+        else
+            ans[1] ^= nums[i];
+    }
+
+    printf("%d %d\n", ans[0], ans[1]);
+
+}
+
+int main(){
+    int n[] = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,81,4742};
+    int len = sizeof(n) / sizeof(n[0]);
+
+    //int ans = singleNumber(n, len);
+    singleNumberIII(n, len);
+    //printf("%d", ans);
+
+}
