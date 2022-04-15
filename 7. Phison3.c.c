7fcb7a16a3769cc377cb3e02a8420c4449802b65
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-15 15:45:21 +0800

    Add files via upload

---
+++ 7. Phison3.c.c
@@ -0,0 +1,36 @@
+/*∏s¡p3√D-1*/
+#include<stdio.h>
+
+void phisonFitst(int *, int);
+void phisonSecond(int *, int, int);
+
+
+int main(){
+
+}
+
+void phisonFitst(int *arr, int len){
+    for(int i = 0; i <= 500; i++){
+        if(i == *(arr))
+            arr++;
+        else
+            printf("%d ", i);
+    }
+    printf("\n");
+}
+
+void phisonSecond(int *arr, int len, int b){
+    while(*arr < b * 100)
+        arr++;
+
+    for(int i = b * 100; i < (b+1)*100; i++){
+        if(*(arr) == i)
+            arr++;
+
+        else
+            printf("%d ", i);
+
+    }
+    printf("\n");
+}
+
