d6dfd04057318c142187b70b7c5712cd17a7111d
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-16 13:11:43 +0800

    Update Binary search and its application.c

--- Binary search and its application.c
+++ Binary search and its application.c
@@ -36,6 +36,29 @@ int binarySearch(int *arr, int len, int target){
     return -1;
 }
 
+/*find the 1, 0 bounadry*/
+/*given the sorted array that contains only 1 & 0*/
+/* {1,1,1,0,0} */
+/* you should find the first 0 index in this array*/
+int binarySearchIII(int *arr, int len){
+    int l = 0;
+    int r = len - 1;
+
+    while(l <= r){
+        int mid = l + (r - l)/2;
+
+        if(arr[mid] == 0 && arr[mid-1] == 1)
+            return mid;
+
+        else if(arr[mid] == 0 && arr[mid-1] == 0)
+            r = mid - 1;
+        else if(arr[mid] == 1)
+            l = mid + 1;
+    }
+
+    return (arr[l-1] == 1)?-1:0;
+}
+
 /*find the index. if not exisit, return the smaller number*/
 int binarySearchII(int *arr, int len, int target){
     int l = 0;
