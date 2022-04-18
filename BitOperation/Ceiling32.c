d170c48c5de83ba80dbb1f4dd45bbda463d0f7de
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-04-18 12:33:20 +0800

    Update Ceiling32.c

--- BitOperation/Ceiling32.c
+++ BitOperation/Ceiling32.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include <stdint.h>
 
-/*ceiling to 32:*/
-/*if 31-> 32*/
-/*if 32-> 32*/
-/*if 67-> 96*/
-/*if 154-> 160*/
+/* ceiling to 32: */
+/* if 31-> 32 */
+/* if 32-> 32 */
+/* if 67-> 96 */
+/* if 154-> 160 */
 
 int ceiling32(int n){
     return ((((n - 1)>>5)+1)<<5);
 }
 
-/*Naive Method*/
+/* Naive Method */
 int ceiling32II(int n){
     int i = 1;
     int ans = 0;
