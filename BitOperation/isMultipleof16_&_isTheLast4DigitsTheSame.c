50ad238e970ad0c0027bc280b478512aad79b863
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-05-10 11:40:35 +0800

    Update isMultipleof16_&_isTheLast4DigitsTheSame.c

--- BitOperation/isMultipleof16_&_isTheLast4DigitsTheSame.c
+++ BitOperation/isMultipleof16_&_isTheLast4DigitsTheSame.c
@@ -9,19 +9,19 @@ int main(){
     return 0;
 }
 
-/*N is Multiplied by 16*/
+/* N is Multiplied by 16 */
 void isMultipleof16(int n){
     printf("%d is multiple of 16: %s\n", n, (n >= 16 && !(n & 15))?"True":"False");
 }
 
-/*給你一個unsigned short(0~65535)，問換算成16進制後四個值是不是一樣，是回傳1，否則0。例如：0xFFFF return 1, 0xAAAB return 0*/
+/* 給你一個unsigned short(0~65535)，問換算成16進制後四個值是不是一樣，是回傳1，否則0。例如：0xFFFF return 1, 0xAAAB return 0 */
 /* Naive method: use the mask left shift four time and check */
 bool is16theSame(unsigned short n){
-    /*use the mask to extract the last 4 digits*/
+    /* Use the mask to extract the last 4 digits */
     int mask = n&15;
     int i = 0;
 
-    /*use the mask to toggle the different digit in the given N*/
+    /* Use the mask to toggle the different digit in the given N */
     while(i < 4){
         n = n ^ mask;
         mask <<= 4;
@@ -31,11 +31,11 @@ bool is16theSame(unsigned short n){
     return n == 0;
 }
 
-/*給你一個unsigned short(0~65535)，問換算成16進制後四個值是不是一樣，是回傳1，否則0。例如：0xFFFF return 1, 0xAAAB return 0*/
+/* 給你一個unsigned short(0~65535)，問換算成16進制後四個值是不是一樣，是回傳1，否則0。例如：0xFFFF return 1, 0xAAAB return 0 */
 /* Left shift the mask directly. */
 
 bool is16theSameII(unsigned short n){
-    /*use the mask to extract the last 4 digits*/
+    /* Use the mask to extract the last 4 digits */
     uint16_t mask = n & 15;
     
     mask = (mask << 12) + (mask << 8) + (mask << 4) + mask;
