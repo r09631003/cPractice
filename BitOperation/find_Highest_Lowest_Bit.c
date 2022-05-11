69faa80b45b2b36bed77f1d58f213e820fe0c52d
Cheng, Tzu-Yang <81865943+r09631003@users.noreply.github.com>
2022-05-11 17:20:16 +0800

    Update find_Highest_Lowest_Bit.c

--- BitOperation/find_Highest_Lowest_Bit.c
+++ BitOperation/find_Highest_Lowest_Bit.c
@@ -4,7 +4,7 @@
 #include<stdint-gcc.h>
 #define setBit1(N, n) ((N)|=(1<<(n)))
 
-/*Naive Method*/
+/* Naive Method */
 void findHighestBit(unsigned int n){
     int x = 31;
     uint32_t mask = 1 << 31;
@@ -18,7 +18,7 @@ void findHighestBit(unsigned int n){
 }
 
 
-/*Better method*/
+/* Better method */
 void findHighestBitII(unsigned int n){
     /*set the highest bit(and the lower bits) to 1 */
     n |= n >> 1;
@@ -27,14 +27,14 @@ void findHighestBitII(unsigned int n){
     n |= n >> 8;
     n |= n >> 16;
 
-    /*ex: 111 - 11; we can get 100, which is the highest bit of 111*/
+    /* ex: 111 - 11; we can get 100, which is the highest bit of 111 */
     printf("%ud\n", n - (n >> 1));
 }
 
-/*Lowest order of bit*/
+/* Lowest order of bit */
 void findLowestBit(unsigned int n){
-    /*-n: is the 2 complement of n*/
-    /*n & (-n) can get the lowest bit(right most bit)*/
+    /* -n: is the 2 complement of n */
+    /* n & (-n) can get the lowest bit(right most bit) */
     int tmp = n & (-n);
 
     printf("%d\n", tmp);
